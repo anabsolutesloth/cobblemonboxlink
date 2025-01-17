340ff187af7615a5e83fddca735424423d2fb3e3
anabsolutesloth <63694982+anabsolutesloth@users.noreply.github.com>
2025-01-16 16:59:00 -0800

    Remove logging from Curios compat

--- src/main/java/com/emperdog/boxlink/compat/CuriosCompat.java
+++ src/main/java/com/emperdog/boxlink/compat/CuriosCompat.java
@@ -6,11 +6,8 @@ import top.theillusivec4.curios.api.CuriosApi;
 
 public class CuriosCompat {
     public static boolean hasBoxLinkAsCurio(Player player) {
-        BoxLinkMod.LOGGER.info("Looking for Box Link in Curio");
-        BoxLinkMod.LOGGER.info("Curios Inventory available: {}", CuriosApi.getCuriosInventory(player).isPresent());
         if(CuriosApi.getCuriosInventory(player).isEmpty())
             return false;
-        BoxLinkMod.LOGGER.info("Box Link found in Curio: {}", !CuriosApi.getCuriosInventory(player).get().findCurios(BoxLinkMod.BOX_LINK_ITEM.get()).isEmpty());
         return !CuriosApi.getCuriosInventory(player).get().findCurios(BoxLinkMod.BOX_LINK_ITEM.get()).isEmpty();
     }
 }
